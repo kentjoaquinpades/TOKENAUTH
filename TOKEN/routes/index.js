384e7b97fb71c2901330bd8d0407f896db628fc4
cskiller24 <ccadayong2020@plm.edu.ph>
2024-03-04 13:31:33 +0800

    Added test

--- TOKEN/routes/index.js
+++ TOKEN/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
+import { db } from "../config/Database.js";
  
 const router = express.Router();
  
@@ -10,6 +11,11 @@ router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+router.get('/test', async (req, res) => {
+    const [results, metadata] = await db.query("SELECT * FROM `hydra.login`"); 
+    
+    res.send(results)
+});
 
  
 export default router;
